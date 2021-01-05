cfee7ae59631647239e3e4c209d78cceb8f37899
Deividas <deividas@Deividass-MacBook-Pro.local>
2021-01-05 18:33:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sup! Boi
         </p>
         <a
           className="App-link"
